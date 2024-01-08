706b1f6546b49d59f3040107988ebc1d9200d93a
habibaaborehab <Habibaaborehab113@gmail.com>
2024-01-08 22:48:36 +0200

    Answering All The Tasks

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-def no_c(myString):
-    theResult = ""
-    for Character in myString:
-        if Character not in "cC":
-            theResult += Character
-    return theResult
+def add_tuple(tuple_a=(), tuple_b=()):
+    a = tuple_a + (0, 0)
+    b = tuple_b + (0, 0)
+
+    result = (a[0] + b[0], a[1] + b[1])
+    return result
