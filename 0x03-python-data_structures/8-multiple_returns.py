efc9965a4740562a55b2ebea324f1eedb4160cd7
habibaaborehab <Habibaaborehab113@gmail.com>
2024-01-08 22:49:24 +0200

    Answering All The Tasks

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    total = len(sentence)
-    char = sentence[0] if total > 0 else "None"
-    new = total, char
-    return(new)
+    if not sentence:
+        return (0, None)
+
+    theLength = len(sentence)
+    firstCharacter = sentence[0]
+    return (theLength, firstCharacter)
